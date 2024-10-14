2c51742be23a0e887135d133b5d9c65013e31e7b
surya <117023142+Suryaxoxz@users.noreply.github.com>
2024-10-14 11:57:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.remove("open-popup");
+}
